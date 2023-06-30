a23fdb6d2651a162a04b5f2585cc4ca8a8ab533a
Md. Shefat Zeon <mdshefatzeon@gmail.com>
2023-06-30 14:35:08 +0600

    new update

--- src/app/redux/actions/auth.action.js
+++ src/app/redux/actions/auth.action.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { useRouter } from "next/router";
 import axiosInstance from "../helpers/axios";
 import { authConstant } from "./constants";
--- src/app/redux/helpers/axios.js
+++ src/app/redux/helpers/axios.js
@@ -1,3 +1,5 @@
+"use client";
+
 import axios from "axios";
 import { api } from "./urlConfig";
 import store from "../store";
